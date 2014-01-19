86ee1dd358cf39ccfc6355596ba8908f78854b4c
manhlnse02619 <manhlnse02619@fpt.edu.vn>
2014-01-19 16:15:51 +0700

    update

--- src/fu/example/currencyconverter/MainActivity.java
+++ src/fu/example/currencyconverter/MainActivity.java
@@ -1,5 +1,7 @@
 package fu.example.currencyconverter;
 
+import java.text.DecimalFormat;
+import java.text.Format;
 import java.util.Currency;
 import java.util.Locale;
 
@@ -7,6 +9,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.database.CursorWrapper;
 import android.graphics.Color;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -22,8 +26,8 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
 	private double USD = 1;// standard currency
-	private double VND = 20000;// 1 USD
-	private double EUR = 0.8;// 1 USD
+	private double VND = 21276.5957;// 1 USD
+	private double ERU = 1.3531;// 1 USD
 	private Button btConvert = null;
 	private Spinner sp1, sp2;
 	private TextView tvTo;
@@ -34,38 +38,36 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		loadLayout();
-		// Currency USD = Currency.getInstance("USD");
-		// Toast.makeText(getApplicationContext(),
-		// Currency.getInstance(Locale.US).getSymbol(),
-		// Toast.LENGTH_SHORT).show();
+
 	}
 
 	private void loadLayout() {
 		sp1 = (Spinner) findViewById(R.id.spinner1);
-		String[] items1 = new String[] { "USD", "VND", "EUR" };
+		String[] items1 = new String[] { "USD", "VND", "ERU" };
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
 				getApplicationContext(),
 				android.R.layout.simple_spinner_dropdown_item, items1);
 		sp1.setAdapter(adapter);
 
 		sp2 = (Spinner) findViewById(R.id.spinner2);
-		String[] items2 = new String[] { "USD", "VND", "EUR" };
+		String[] items2 = new String[] { "USD", "VND", "ERU" };
 		ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(
 				getApplicationContext(),
 				android.R.layout.simple_spinner_dropdown_item, items2);
 		sp2.setAdapter(adapter2);
-		
+
 		btConvert = (Button) findViewById(R.id.button1);
 		btConvert.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
 				convert();
 			}
 		});
-		
+
 		edFrom = (EditText) findViewById(R.id.editText1);
+
 		tvTo = (TextView) findViewById(R.id.textView3);
 
 	}
@@ -75,7 +77,7 @@ public class MainActivity extends Activity {
 		if (FromCurrency.compareTo("USD") == 0) {
 			from = USD;
 		} else if (FromCurrency.compareTo("ERU") == 0) {
-			from = EUR;
+			from = ERU;
 		} else {
 			from = VND;
 		}
@@ -84,25 +86,27 @@ public class MainActivity extends Activity {
 		if (ToCurrency.compareTo("USD") == 0) {
 			to = USD;
 		} else if (ToCurrency.compareTo("ERU") == 0) {
-			to = EUR;
+			to = ERU;
 		} else {
 			to = VND;
 		}
-		//Toast.makeText(getApplicationContext(), from + " " + to, Toast.LENGTH_SHORT).show();
-		return (to/from);
+
+		return (to / from);
 	}
 
-	private void convert(){
-    	String from = sp1.getSelectedItem().toString();
-    	String to = sp2.getSelectedItem().toString();
-    	
-    	Double exchange = calExchange(from, to);
-    	
-    	Double fromvalue = Double.parseDouble(edFrom.getText().toString());
-    	fromvalue = fromvalue*exchange;
-    	tvTo.setText(fromvalue.toString());
-    	
-    }
+	private void convert() {
+		String from = sp1.getSelectedItem().toString();
+		String to = sp2.getSelectedItem().toString();
+
+		Double exchange = calExchange(from, to);
+		String input = edFrom.getText().toString();
+		Double fromvalue = Double.parseDouble(input);
+		fromvalue = fromvalue * exchange;
+		DecimalFormat format = new DecimalFormat("###,###.##");
+		tvTo.setText(format.format(Double.parseDouble(input)) + " " + from
+				+ "\n = \n" + format.format(fromvalue) + " " + to);
+
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
